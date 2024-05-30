75be444193577a724aed8313ccc161b1c16718dc
Md Ibrahim <faiziibrahim321@gmail.com>
2024-05-30 11:44:54 +0530

    ..

---
+++ myIfelseDay14.py
@@ -0,0 +1,33 @@
+# age=int(input("Enter your age:"))
+# print("Your age is",age)
+
+# if(age>18):
+#     print("Your are eligible for voting") 
+#     # it gives space due to indentation 
+# elif(age<18):
+#     print("You are not eligible for voting")
+# else:
+#     print("your age is 18")
+
+#     print(age>=18)
+#     print(age==18)
+#     print(age<=18)
+#     print(age!=18)
+# appleprice=140
+# budget=200
+
+# if(budget-appleprice>50):
+#         print("I will buy the apple")
+# elif(budget-appleprice>70):
+#         print("it ok to buy")
+# else:
+#         print("Alexa, do not add apples to the cart")
+
+num=int(input("Enter the value of the num:"))
+if(num<0):
+    print("number is negative")
+    
+
+
+
+
