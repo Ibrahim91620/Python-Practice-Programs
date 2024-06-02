2fa25531ed25138a381db1ed5f2fc2d1db115228
Md Ibrahim <faiziibrahim321@gmail.com>
2024-06-02 07:38:35 +0530

    practice

---
+++ myFunction20.py
@@ -0,0 +1,28 @@
+# function
+
+def calculateGmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+a=2
+b=3
+    
+    
+    
+    
+calculateGmean(a,b)
+
+def add(a,b):
+    add=a+b
+    print(add)
+
+c=10
+d=5
+add(c,d)    
+def isgreater(a,b):
+ if(a>b):
+    print("a is greater than b")
+ else:
+    print("b is greater than a")
+a=0
+b=1
+isgreater(a,b)
