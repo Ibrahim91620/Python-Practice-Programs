167cb63a17df7dbd8a79ba92ed2f9977bedeca68
Md Ibrahim <faiziibrahim321@gmail.com>
2024-06-04 07:54:44 +0530

    listmethod

---
+++ MyListMethod23.py
@@ -0,0 +1,18 @@
+l=[2,3,4,5,5]
+# print(l)
+# l.append(3)
+# print(l)
+# l.sort()
+# print(l)
+# l.reverse()
+# print(l)
+# print("the counting of 5 is",l.count(5))
+# print(l)
+
+m=[2000,1000,11000]
+
+# l.extend(m)
+# print(l)
+k=l+m
+print(k)
+
