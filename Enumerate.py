82836940d8d3b37b791d2c95a61fe99338d905b6
Md Ibrahim <faiziibrahim321@gmail.com>
2024-06-19 13:16:28 +0530

    Enumerate

---
+++ Enumerate.py
@@ -0,0 +1,8 @@
+# Enumerate function is a built in function 
+# in Python to Loop Over a sequence such as 
+# list,tuple,or String
+
+fruits=["APple","Banana","Guava"]
+
+for index, fruit in enumerate(fruits):
+    print(index,fruit)
---
+++ MyIfelseshort.py
@@ -0,0 +1,3 @@
+a=1100
+b=1100
+print("A") if a>b else print("=") if a==b else print("B") 
